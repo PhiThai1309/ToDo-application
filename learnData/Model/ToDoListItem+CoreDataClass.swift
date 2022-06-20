8eec3cc64f5922c5268ab07e80224ba487077fc9
Phi Thai <s3878070@rmit.edu.vn>
2022-06-20 14:28:36 +0700

    Initial Commit

---
+++ learnData/Model/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  learnData
+//
+//  Created by Phi Thai on 20/06/2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
---
+++ learnData/Model/ToDoListItem+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  ToDoListItem+CoreDataProperties.swift
+//  learnData
+//
+//  Created by Phi Thai on 20/06/2022.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension ToDoListItem {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ToDoListItem> {
+        return NSFetchRequest<ToDoListItem>(entityName: "ToDoListItem")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var createdAt: Date?
+
+}
+
+extension ToDoListItem : Identifiable {
+
+}
